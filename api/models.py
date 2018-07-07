f7c149002d21a14cbb0145c939071030bc4b34ca
Yun Liu <liuyun.97@gmail.com>
2018-07-07 17:45:43 +0800

    BOOM

--- api/models.py
+++ api/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Userinfo(models.Model):
+class UserInfo(models.Model):
     nickname = models.CharField(max_length=16)
     gender = models.CharField(max_length=16)
     avatar = models.FileField(upload_to='')
     # 用户关注 多对多联系
-    friend = models.ManyToManyField("self", symmetrical=False)
-    info = models.ManyToManyField(User)
+    friends = models.ManyToManyField("self", symmetrical=False)
+    # 用户-菜谱 多对多关系 (收藏关系)
+    recipe_collection = models.ManyToManyField('Recipe')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class Recipe(models.Model):
@@ -20,10 +22,7 @@ class Recipe(models.Model):
     like_count = models.IntegerField(default='0')
     collect_count = models.IntegerField(default='0')
     # 用户-菜谱 一对多联系 (属于关系)
-    create_by = models.ForeignKey(Userinfo, on_delete=models.CASCADE, related_name="create")
-    # 用户-菜谱 多对多关系 (收藏关系)
-    recipe_collection = models.ManyToManyField(Userinfo, through='User_recipe_collection',
-                                               through_fields=('recipe_id', 'user_id'))
+    create_by = models.ForeignKey(UserInfo, on_delete=models.CASCADE, related_name="create")
 
     class Meta:
         ordering = ('created_at',)
@@ -33,7 +32,7 @@ class Tag(models.Model):
     title = models.CharField(max_length=10)
     descrption = models.TextField(max_length=4096, blank=False, default='')
     # 菜谱-标签 多对多联系
-    belong = models.ManyToManyField(Recipe, through='Recipe_tag', through_fields=('tag_id', 'recipe_id'))
+    belong = models.ManyToManyField(Recipe)
 
 
 class Comment(models.Model):
@@ -43,14 +42,4 @@ class Comment(models.Model):
     # 菜谱-评论 一对多联系
     recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     # 用户-评论 一对多联系
-    user_id = models.ForeignKey(Userinfo, on_delete=models.CASCADE)
-
-
-class User_recipe_collection(models.Model):
-    user_id = models.ForeignKey(Userinfo, on_delete=models.CASCADE, related_name="collect")
-    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-
-
-class Recipe_tag(models.Model):
-    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
