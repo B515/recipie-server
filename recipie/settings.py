be399ef0ed81e023fcf1772b038bd8d7e9bb2587
Yun Liu <liuyun.97@gmail.com>
2018-07-08 22:25:15 +0800

    token

--- recipie/settings.py
+++ recipie/settings.py
@@ -129,7 +129,10 @@ SITE_ID = 1
 SESSION_COOKIE_AGE = 2419200
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.SessionAuthentication',),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    ),
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
 }
 
