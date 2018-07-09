e48af35458436d4ca6bf07904733b0f1742c1747
Yun Liu <liuyun.97@gmail.com>
2018-07-09 20:05:03 +0800

    file

--- recipie/urls.py
+++ recipie/urls.py
@@ -13,22 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
 from api import views
+from recipie import settings
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserInfoViewSet)
 router.register(r'recipes', views.RecipeViewSet)
 router.register(r'tags', views.TagViewSet)
-router.register(r'comments',views.CommentViewSet)
+router.register(r'comments', views.CommentViewSet)
+router.register(r'files', views.FileViewSet)
 urlpatterns = [
-    path('', views.index),
-    path('api/', include(router.urls)),
-    path('auth/', include('rest_auth.urls')),
-    path('auth/registration/', include('rest_auth.registration.urls')),
-    path('jet/', include('jet.urls', 'jet')),
-    path('admin/', admin.site.urls),
-]
+                  path('', views.index),
+                  path('api/', include(router.urls)),
+                  path('auth/', include('rest_auth.urls')),
+                  path('auth/registration/', include('rest_auth.registration.urls')),
+                  path('jet/', include('jet.urls', 'jet')),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
