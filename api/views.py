e66ae398d0051652ba7d43d80640fc53f2c1999f
Yun Liu <liuyun.97@gmail.com>
2018-07-09 15:30:47 +0800

    me

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from api.models import Recipe, UserInfo, Tag
 from .serializers import RecipeSerializer, UserInfoSerializer, TagSerializer
@@ -11,6 +13,20 @@ class UserInfoViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(methods=['get'], detail=False)
+    def me(self, request):
+        me = UserInfo.objects.get(user=request.user.id)
+        serializer = self.get_serializer(me)
+        return Response(serializer.data)
+
+    @action(methods=['post'], detail=False)
+    def update_me(self, request):
+        me = UserInfo.objects.get(user=request.user.id)
+        serializer = self.get_serializer(me, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
